e82455dd637e2c4a3d1b73a77b506ffcc6a602fa
arcamp0130 <arcmp0130@gmail.com>
2025-10-07 23:00:27 -0600

    feat: creating express main configuration and importing .env keys

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello from Node!")
+import express from "express"
+import { fileURLToPath } from "url"
+import { PORT, ENVIROMENT } from "./secrets.ts"
+import path from "path"
+
+const app = express()
+
+// Get current directory from relative url path
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// Solving static path to serve files
+const staticPath = ENVIROMENT === "production"
+    ? path.join(__dirname, "../dist/public")    // prod
+    : path.join(__dirname, "../public")         // dev
+
+const router = express.Router()
+
+// router to root
+router.get("/", (_: express.Request, res: express.Response) => {
+    res.sendFile("index.html", { root: "public" })
+})
+
+// using express configurations
+app.use(express.static(staticPath))
+app.use("/", router);
+
+// launching server
+app.listen(PORT, () => {
+    console.log(`${ENVIROMENT} server's running at http://localhost:${PORT}`)
+})
