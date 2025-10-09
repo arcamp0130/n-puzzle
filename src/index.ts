7711cba9fdb5966d039f9068e12b8d3e33ad35a0
arcamp0130 <arcmp0130@gmail.com>
2025-10-08 22:17:47 -0600

    refactor: importing routers and styling code.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express"
-import { fileURLToPath } from "url"
-import { PORT, ENVIROMENT } from "./secrets.ts"
 import path from "path"
+import { fileURLToPath } from "url"
+import { router } from "./routes/index.route.ts"
+import { PORT, ENVIROMENT } from "./secrets.ts"
 
 const app = express()
 
@@ -12,13 +13,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const staticPath = ENVIROMENT === "production"
     ? path.join(__dirname, "../dist/public")    // prod
     : path.join(__dirname, "../public")         // dev
-
-const router = express.Router()
-
-// router to root
-router.get("/", (_: express.Request, res: express.Response) => {
-    res.sendFile("index.html", { root: "public" })
-})
 
 // using express configurations
 app.use(express.static(staticPath))
--- src/secrets.ts
+++ src/secrets.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv"
 config()
 
-export const PORT               =       process.env.PORT || 8080
-export const ENVIROMENT         =       process.env.NODE_ENV || "development"
+export const PORT               =       process.env.PORT        || 8080
+export const ENVIROMENT         =       process.env.NODE_ENV    || "development"
