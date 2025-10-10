dc36e4a0b57bdab6a9def5b658d12532f6003dac
arcamp0130 <arcmp0130@gmail.com>
2025-10-10 00:16:32 -0600

    feat: generating n*n board with game tokens.

--- public/ts/managers/html.manager.ts
+++ public/ts/managers/html.manager.ts
@@ -1,6 +1,7 @@
 export default class htmlManager {
     private static instance: htmlManager
     private board: HTMLElement
+    private boardSize: number = 4
     public readonly buttons: { [key: string]: HTMLButtonElement }
 
     // Private constructor to prevent direct instantiation
@@ -14,15 +15,40 @@ export default class htmlManager {
         this.init()
     }
 
-    private init() {
-        console.log("htmlManager initialized")
-        this.board.innerHTML = ""
-    }
-
     public static get Instance(): htmlManager {
         if (!htmlManager.instance) {
             htmlManager.instance = new htmlManager()
         }
         return htmlManager.instance
+    }
+
+    private init(): void {
+        this.generateGame()
+    }
+
+    private generateGame(): void {
+        const slot = document.createElement("span")
+        slot.classList.add("slot")
+        this.board.innerHTML = ""
+
+        // Generating gameboard
+        for (let i = 0; i < this.boardSize; i++) { // y possition
+            for (let j = 0; j < this.boardSize; j++) { // x possition
+                const index = (i * this.boardSize) + (j + 1)
+                slot.innerHTML = ""
+                slot.dataset.y = `${i}`
+                slot.dataset.x = `${j}`
+
+                if (index !== this.boardSize * this.boardSize) {
+                    slot.innerHTML = `${index}`
+                    slot.dataset.status = "fill"
+                } else {
+                    slot.dataset.status = "empty"
+                }
+
+                this.board.appendChild(slot.cloneNode(true))
+
+            } // end x pos
+        } // end y pos
     }
 }
