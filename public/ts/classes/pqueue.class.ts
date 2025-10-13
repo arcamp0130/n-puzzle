94bc451f49ac0d1c1383daaec8b55827e54324b7
arcamp0130 <arcmp0130@gmail.com>
2025-10-12 19:03:56 -0600

    feat: creating empty methods and helpers to PQueue.

--- public/ts/classes/pqueue.class.ts
+++ public/ts/classes/pqueue.class.ts
@@ -3,5 +3,38 @@ import { PQueueItem } from "../types/game.types"
 export default class PQueue<T> {
     private items: PQueueItem<T>[] = []
 
-    constructor() {}
+    constructor() { }
+
+    public enqueue(element: T, cost: number): void {
+
+    }
+
+    public dequeue(): void {
+
+    }
+
+    public peek(): void {
+
+    }
+
+    public contains(): void {
+
+    }
+
+    public updatePriotity(): void {
+        
+    }
+
+    public size(): number {
+        return this.items.length
+    }
+
+    public isEmpty(): boolean {
+        return this.items.length === 0
+    }
+
+    public clear(): void {
+        this.items = []
+    }
+
 }
